104830887799a3c37b5105a8750569e8e48a13b7
Aldrich Lapid <alapid@LABA.PSWAS.LOCAL>
2023-04-12 15:50:24 -0700

    revised final draft

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let heading = document.querySelector("h1");
 let hideButton = document.getElementById("hide-button");
 let showButton = document.getElementById("show-button");
 
+heading.textContent="Header 1"
+
 hideButton.addEventListener("click", () => {
   heading.style.display = "none";
 });
